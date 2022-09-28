ccaf32280ddd43a25d4a3c45278a657fddf91c65
Rahmad Juanda Patra <patrajuanda10@gmail.com>
2022-09-28 17:48:46 +0700

    first init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app =express()
+require('dotenv').config()
+app.use(express.json())
+const PORT = process.env.PORT || 2000
+
+app.get('/', (req, res)=>{
+    res.status(200).send(`<h1> Welcome to Node CI CD Apps </h1>`)
+})
+
+app.get('/demo', (req,res)=>{
+    res.status(200).sed(`<h1>Dsasasemo page</h1>`)
+})
+
+
+app.listen(PORT, ()=> console.log(`magic happens in PORT : ${PORT}`))
